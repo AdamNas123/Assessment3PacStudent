445bda3f1b0b2802da437f0067bc1ce43b70ff5b
Adam Nasser <adam.nasser246@gmail.com>
2022-09-13 21:30:24 +1000

    Added PlayAudio script

---
+++ Assets/Scripts/PlayAudio.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudio : MonoBehaviour
+{
+    public AudioSource introSource;
+    public AudioSource normalStateSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        introSource.Play();
+        normalStateSource.PlayDelayed(introSource.clip.length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
