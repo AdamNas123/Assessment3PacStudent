daa1594327d9b458ac6b206872d79d1b0d351839
Adam Nasser <adam.nasser246@gmail.com>
2022-09-23 23:55:37 +1000

    Added code for tweener

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -13,27 +13,28 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float elapsedTime = Time.time - tween.StartTime;
-        float t = elapsedTime / tween.Duration;
+        
 
-        if (gameObject.transform.position.Equals(new Vector3(-16.5f, -1.5f, 0.0f)))
-        {
-            
+        //if (gameObject.transform.position.Equals(new Vector3(-16.5f, -1.5f, 0.0f)))
+        //{
+            AddTween(gameObject.transform, gameObject.transform.position, new Vector3(-11.5f, -1.5f, 0.0f), 1.5f);
+            float elapsedTime = Time.time - tween.StartTime;
+            float t = elapsedTime / tween.Duration;
             tween.Target.position = Vector3.Lerp(tween.StartPos, tween.EndPos, t);
-        }   
+       //}   
     }
 
     public bool AddTween(Transform targetObject, Vector3 startPos, Vector3 endPos, float duration)
     {
-        if (!TweenExists(targetObject))
-        {
+        //if (!TweenExists(targetObject))
+        //{
             tween = new Tween(targetObject, startPos, endPos, Time.time, duration);
             return true;
-        }
-        else
-        {
-            return false;
-        }
+        //}
+        //else
+       // {
+       //     return false;
+       // }
     }
 
     public bool TweenExists(Transform target)
