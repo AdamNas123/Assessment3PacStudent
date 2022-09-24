bffd38230c7a766cce255eddb611162f05504003
Adam Nasser <adam.nasser246@gmail.com>
2022-09-24 12:17:56 +1000

    Added initial pacstudent right movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,7 +7,9 @@ public class Movement : MonoBehaviour
     private Tween tween;
     // Start is called before the first frame update
     void Start()
-    {  
+    {
+        tween = new Tween(gameObject.transform, gameObject.transform.position, new Vector3(-11.5f, -1.5f, 0.0f), Time.time, 1.5f);
+        //AddTween(gameObject.transform, gameObject.transform.position, new Vector3(-11.5f, -1.5f, 0.0f), 1.5f);
     }
 
     // Update is called once per frame
@@ -17,24 +19,37 @@ public class Movement : MonoBehaviour
 
         //if (gameObject.transform.position.Equals(new Vector3(-16.5f, -1.5f, 0.0f)))
         //{
-            AddTween(gameObject.transform, gameObject.transform.position, new Vector3(-11.5f, -1.5f, 0.0f), 1.5f);
+        //    AddTween(gameObject.transform, gameObject.transform.position, new Vector3(-11.5f, -1.5f, 0.0f), 1.5f);
+        //    float elapsedTime = Time.time - tween.StartTime;
+        //    float t = elapsedTime / tween.Duration;
+        //    tween.Target.position = Vector3.Lerp(tween.StartPos, tween.EndPos, t);
+        //}   
+        if (tween != null && Vector3.Distance(tween.Target.position, tween.EndPos) > 0.1f)
+        {
             float elapsedTime = Time.time - tween.StartTime;
             float t = elapsedTime / tween.Duration;
             tween.Target.position = Vector3.Lerp(tween.StartPos, tween.EndPos, t);
-       //}   
+
+        }
+        else if (tween != null)
+        {
+            tween.Target.position = tween.EndPos;
+            tween = null;
+            //activeTweens.Remove(activeTweens[i]);
+        }
     }
 
     public bool AddTween(Transform targetObject, Vector3 startPos, Vector3 endPos, float duration)
     {
-        //if (!TweenExists(targetObject))
-        //{
+        if (!TweenExists(targetObject))
+        {
             tween = new Tween(targetObject, startPos, endPos, Time.time, duration);
             return true;
-        //}
-        //else
-       // {
-       //     return false;
-       // }
+        }
+        else
+        {
+            return false;
+        }
     }
 
     public bool TweenExists(Transform target)
