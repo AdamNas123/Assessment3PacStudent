f37fe47e5280fe0cafe24284986337fd0d3b4474
Adam Nasser <adam.nasser246@gmail.com>
2022-10-26 21:53:23 +1100

    Fixed pacstudent movement

--- Assets/Scripts/PacStudentController.cs
+++ Assets/Scripts/PacStudentController.cs
@@ -35,15 +35,44 @@ public class PacStudentController : MonoBehaviour
         {2,2,2,2,2,1,5,3,3,0,4,0,0,0},
         {0,0,0,0,0,0,5,0,0,0,4,0,0,0},
     };
+    private int[,] fullLevelMap;
 
-    private bool[,] pacPosition = new bool[15, 14];
+    //private bool[,] pacPosition = new bool[15, 14];
     private int xPos = 1;
     private int yPos = 1;
     
     // Start is called before the first frame update
     void Start()
     {
-        pacPosition[xPos, yPos] = true;
+        //Extend level map to full map
+        fullLevelMap = new int[2 * levelMap.GetLength(0) - 1, 2 * levelMap.GetLength(1)];
+
+        for (int i = 0; i < fullLevelMap.GetLength(0); ++i)
+        {
+            for (int j = 0; j < fullLevelMap.GetLength(1); ++j)
+            {
+                if (i < levelMap.GetLength(0) && j < levelMap.GetLength(1))
+                {
+                    fullLevelMap[i, j] = levelMap[i, j];
+                }
+
+                if (i < levelMap.GetLength(0) && j >= levelMap.GetLength(1))
+                {
+                    fullLevelMap[i, j] = levelMap[i, fullLevelMap.GetLength(1) - j - 1];
+                }
+
+                if (i >= levelMap.GetLength(0) && j < levelMap.GetLength(1))
+                {
+                    fullLevelMap[i, j] = levelMap[fullLevelMap.GetLength(0) - i - 1, j];
+                }
+
+                if (i >= levelMap.GetLength(0) && j >= levelMap.GetLength(1))
+                {
+                    fullLevelMap[i, j] = levelMap[fullLevelMap.GetLength(0) - i - 1, fullLevelMap.GetLength(1) - j - 1];
+                }
+            }
+        }
+
         pacStudentAnimator = GetComponent<Animator>();
         movingNoPellet = GetComponent<AudioSource>();
         //activeTween = new Tween(gameObject.transform, new Vector3(-16.5f, -1.5f, 0.0f), new Vector3(-11.5f, -1.5f, 0.0f), Time.time, 1.5f);
@@ -54,25 +83,95 @@ public class PacStudentController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float leftPos = gameObject.transform.position.x - 1;
+        /*float leftPos = gameObject.transform.position.x - 1;
         float rightPos = gameObject.transform.position.x + 1;
         float downPos = gameObject.transform.position.y - 1;
-        float upPos = gameObject.transform.position.y + 1;
+        float upPos = gameObject.transform.position.y + 1; */
 
-        if (Input.GetKeyDown("a") || (lastInput == "a" && !tweener.TweenExists(gameObject.transform)))
+        if (Input.GetKeyDown("a"))
+        {
+            lastInput = "a";
+        }
+        if (Input.GetKeyDown("d"))
+        {
+            lastInput = "d";
+        }
+        if (Input.GetKeyDown("s"))
+        {
+            lastInput = "s";
+        }
+        if (Input.GetKeyDown("w"))
+        {
+            lastInput = "w";
+        }
+        if (!tweener.TweenExists(gameObject.transform))
+        {
+            if (lastInput == "a")
+            {
+                if (isWalkable(yPos, xPos - 1))
+                {
+                    currentInput = lastInput;
+                    tweener.AddTween(gameObject.transform, gameObject.transform.position, new Vector3(gameObject.transform.position.x - 1, gameObject.transform.position.y, 0.0f), 0.5f);
+                    xPos -= 1;
+                }
+                else
+                {
+                    checkCurrentInput(currentInput);
+                }
+            }
+            else if (lastInput == "d")
+            {
+                if (isWalkable(yPos, xPos + 1))
+                {
+                    currentInput = lastInput;
+                    tweener.AddTween(gameObject.transform, gameObject.transform.position, new Vector3(gameObject.transform.position.x + 1, gameObject.transform.position.y, 0.0f), 0.5f);
+                    xPos += 1;
+                }
+                else
+                {
+                    checkCurrentInput(currentInput);
+                }
+            }
+            else if (lastInput == "s")
+            {
+                if (isWalkable(yPos + 1, xPos))
+                {
+                    currentInput = lastInput;
+                    tweener.AddTween(gameObject.transform, gameObject.transform.position, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - 1, 0.0f), 0.5f);
+                    yPos += 1;
+                }
+                else
+                {
+                    checkCurrentInput(currentInput);
+                }
+            }
+            else if (lastInput == "w")
+            {
+                if (isWalkable(yPos - 1, xPos))
+                {
+                    currentInput = lastInput;
+                    tweener.AddTween(gameObject.transform, gameObject.transform.position, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 1, 0.0f), 0.5f);
+                    yPos -= 1;
+                }
+                else
+                {
+                    checkCurrentInput(currentInput);
+                }
+            }
+        }
+    }
+          /*  if (Input.GetKeyDown("a") || (lastInput == "a" && !tweener.TweenExists(gameObject.transform)))
         {
             if (isWalkable(yPos, xPos - 1))
             {
                 currentInput = lastInput;
                 tweener.AddTween(gameObject.transform, gameObject.transform.position, new Vector3(gameObject.transform.position.x - 1, gameObject.transform.position.y, 0.0f), 0.5f);
                 lastInput = "a";
-                pacPosition[xPos, yPos] = false;
                 xPos -= 1;
-                pacPosition[xPos, yPos] = true;
             }
             else
             {
-                
+                checkCurrentInput(currentInput);
             }
         }
 
@@ -83,12 +182,11 @@ public class PacStudentController : MonoBehaviour
                 currentInput = lastInput;
                 tweener.AddTween(gameObject.transform, gameObject.transform.position, new Vector3(gameObject.transform.position.x + 1, gameObject.transform.position.y, 0.0f), 0.5f);
                 lastInput = "d";
-                pacPosition[xPos, yPos] = false;
                 xPos += 1;
-                pacPosition[xPos, yPos] = true;
             }
-            else {
-
+            else 
+            {
+                checkCurrentInput(currentInput);
             }
         }
 
@@ -99,9 +197,11 @@ public class PacStudentController : MonoBehaviour
                 currentInput = lastInput;
                 tweener.AddTween(gameObject.transform, gameObject.transform.position, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - 1, 0.0f), 0.5f);
                 lastInput = "s";
-                pacPosition[xPos, yPos] = false;
                 yPos += 1;
-                pacPosition[xPos, yPos] = true;
+            }
+            else
+            {
+                checkCurrentInput(currentInput);
             }
         }
 
@@ -112,27 +212,48 @@ public class PacStudentController : MonoBehaviour
                 currentInput = lastInput;
                 tweener.AddTween(gameObject.transform, gameObject.transform.position, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 1, 0.0f), 0.5f);
                 lastInput = "w";
-                pacPosition[xPos, yPos] = false;
                 yPos -= 1;
-                pacPosition[xPos, yPos] = true;
+            }
+            else
+            {
+                checkCurrentInput(currentInput);
             }
         }
-
-        if (!tweener.TweenExists(gameObject.transform))
-        {
-            //Check if walkable    
-        }
-    }
+    } */
 
     private bool isWalkable(int yPos, int xPos)
     {
-        if (xPos < 0 || xPos == levelMap.GetLength(1) || yPos < 0 || yPos == levelMap.GetLength(0) || walls.Contains(levelMap[yPos, xPos]))
+        if (xPos < 0 || xPos == fullLevelMap.GetLength(1) || yPos < 0 || yPos == fullLevelMap.GetLength(0) || yPos == 12 && (xPos == 13 || xPos == 14) || yPos == 16 && (xPos == 13 || xPos == 14) || walls.Contains(fullLevelMap[yPos, xPos]))
         {
             return false;
         }
         else 
         {
             return true;
+        }
+    }
+
+    private void checkCurrentInput(string currentInput)
+    {
+        if (currentInput == "a" && isWalkable(yPos, xPos - 1))
+        {
+            tweener.AddTween(gameObject.transform, gameObject.transform.position, new Vector3(gameObject.transform.position.x - 1, gameObject.transform.position.y, 0.0f), 0.5f);
+            xPos -= 1;
+        }
+        if (currentInput == "d" && isWalkable(yPos, xPos + 1))
+        {
+            tweener.AddTween(gameObject.transform, gameObject.transform.position, new Vector3(gameObject.transform.position.x + 1, gameObject.transform.position.y, 0.0f), 0.5f);
+            xPos += 1;
+        }
+        if (currentInput == "s" && isWalkable(yPos + 1, xPos))
+        {
+            tweener.AddTween(gameObject.transform, gameObject.transform.position, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - 1, 0.0f), 0.5f);
+            yPos += 1;
+        }
+        if (currentInput == "w" && isWalkable(yPos - 1, xPos))
+        {
+            tweener.AddTween(gameObject.transform, gameObject.transform.position, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 1, 0.0f), 0.5f);
+            yPos -= 1;
         }
     }
 }
