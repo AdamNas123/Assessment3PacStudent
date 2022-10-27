cef5eadd586135e39d38f2bb49c651493f8036ca
Adam Nasser <adam.nasser246@gmail.com>
2022-10-27 20:22:40 +1100

    Fixed cherry movement

--- Assets/Scripts/CherryController.cs
+++ Assets/Scripts/CherryController.cs
@@ -9,13 +9,13 @@ public class CherryController : MonoBehaviour
     [SerializeField]
     private GameObject cherry;
     private GameObject currentCherry;
-    private string randomPos;
+    private string[] positions = { "x", "y" }; 
     private float xPos;
     private float yPos;
     // Start is called before the first frame update
     void Start()
     {
-        randomPos = "x";    
+       
     }
 
     // Update is called once per frame
@@ -34,35 +34,35 @@ public class CherryController : MonoBehaviour
     private void SpawnCherry()
     {
         Vector3 endPos;
+        string randomPos = positions[Random.Range(0, 2)];
+        float[] borderPos = { -0.1f, 1.1f };
         if (randomPos == "x")
         {
             xPos = Random.Range(0.0f, 1.0f);
-            yPos = Random.Range(0, 2);
-            if (yPos == 0)
+            yPos = borderPos[Random.Range(0, 2)];
+            if (yPos == -0.1f)
             {
-                endPos = new Vector3(1.0f - xPos, 1.0f, 1.0f);
+                endPos = new Vector3(1.0f - xPos, 1.1f, 1.0f);
             }
             else
             {
-                endPos = new Vector3(1.0f - xPos, 0.0f, 1.0f);
+                endPos = new Vector3(1.0f - xPos, -0.1f, 1.0f);
             }
-            randomPos = "y";
         }
         else 
         {
             yPos = Random.Range(0.0f, 1.0f);
-            xPos = Random.Range(0, 2);
-            if (xPos == 0)
+            xPos = borderPos[Random.Range(0, 2)];
+            if (xPos == -0.1f)
             {
-                endPos = new Vector3(1.0f, 1.0f - yPos, 1.0f);
+                endPos = new Vector3(1.1f, 1.0f - yPos, 1.0f);
             }
             else
             {
-                endPos = new Vector3(0.0f, 1.0f - yPos, 1.0f);
+                endPos = new Vector3(-0.1f, 1.0f - yPos, 1.0f);
             }
-            randomPos = "x";
         }
-        //Vector3 spawnPos = new Vector3(xPos, yPos, 0.0f);
+
         Vector3 spawnPos = Camera.main.ViewportToWorldPoint(new Vector3(xPos, yPos, 1.0f));
         currentCherry = Instantiate(cherry, spawnPos, Quaternion.identity);
         Vector3 worldEndPos = Camera.main.ViewportToWorldPoint(endPos);
